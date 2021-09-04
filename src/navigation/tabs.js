dd7b038b4199bb7b894ea1f0182306a22b25c245
qasimgit <qasimpkkhi@gmail.com>
2021-09-05 00:53:29 +0500

    bottom tabs navigatio not showing

---
+++ src/navigation/tabs.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Image } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Home } from '../screens'
+import { COLORS, icons } from '../constants'
+
+const Tab = createBottomTabNavigator()
+
+const tabOptions = {
+    showLabel: false,
+    style: {
+        height: '10%',
+        backgroundColor: COLORS.black
+    }
+}
+
+const Tabs = () => {
+    return (
+        <Tab.Navigator
+            tabBar={tabOptions}
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused }) => {
+                    const tintColor = focused ? COLORS.white : COLORS.black
+
+                    switch (route.name) {
+                        case "Home":
+                            return (
+                                <Image source={icons.dashboard_icon} resizeMode="contain" style={{ tintColor: tintColor, width: 25, height: 25 }} />
+                            )
+                        case "Search":
+                            return (
+                                <Image source={icons.search_icon} resizeMode="contain" style={{ tintColor: tintColor, width: 25, height: 25 }} />
+                            )
+                        case "Notication":
+                            return (
+                                <Image source={icons.notification_icon} resizeMode="contain" style={{ tintColor: tintColor, width: 25, height: 25 }} />
+                            )
+                        case "Setting":
+                            return (
+                                <Image source={icons.menu_icon} resizeMode="contain" style={{ tintColor: tintColor, width: 25, height: 25 }} />
+                            )
+                    }
+                }
+            })}
+        >
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="Search" component={Home} />
+            <Tab.Screen name="Notification" component={Home} />
+            <Tab.Screen name="Setting" component={Home} />
+
+        </Tab.Navigator>
+    )
+}
+
+export default Tabs
